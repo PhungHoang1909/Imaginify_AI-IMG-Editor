3c2944ebfd656a4c2525f0bd1d683fabf8fa76e5
phung hoang <lnphunghoang190903@gmail.com>
2024-04-12 16:34:30 +0700

    Add Image Form

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,8 +1,108 @@
-import React from 'react'
+"use client"
 
-const TransformationForm = () => {
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+import { Input } from "@/components/ui/input"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
+import { CustomField } from "./CustomField"
+import { useState } from "react"
+import { AspectRatioKey } from "@/lib/utils"
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+})
+
+
+const TransformationForm = ({ action, data = null, userId, type, creditBalance } : TransformationFormProps) => {
+
+  const transformationType = transformationTypes[type];
+  const [Image, setImage] = useState(data)
+  const [newTransformation, setnewTransformation] = useState<Transformations | null>(null)
+  const initialValues = data && action === 'Update' ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  const onSelectFieldHandle = (value: string, onChangeField: (value: string) => void) => {
+
+  }
+
   return (
-    <div>TransformationForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField 
+          control={form.control}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({field}) => <Input {...field} className="input-field" />}
+        />
+
+        {type === 'fill' && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({field}) => (
+          <Select
+              onValueChange={(value) => onSelectFieldHandle(value, field.onChange)}
+          >
+            <SelectTrigger className="select-field">
+              <SelectValue placeholder="Select sizer" />
+            </SelectTrigger>
+            <SelectContent>
+              {Object.keys(aspectRatioOptions).map((key) => (
+                <SelectItem key={key} value={key} className="select-itiem">
+                  {aspectRatioOptions[key as AspectRatioKey].label}
+                </SelectItem>
+              ) )}
+            </SelectContent>
+          </Select>
+            )}
+          />
+        )}
+      </form>
+    </Form>
   )
 }
 
